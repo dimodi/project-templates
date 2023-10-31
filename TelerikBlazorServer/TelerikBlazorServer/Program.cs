b036cd60f767119ba8d118160d133a39dafe7606
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2023-10-31 22:50:24 +0200

    add Telerik Blazor Server and WebAssembly solution templates

---
+++ TelerikBlazorServer/TelerikBlazorServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.SignalR;
+using TelerikBlazorServer.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddTelerikBlazor();
+
+// Upload file size limit on ASP.NET Core
+builder.Services.Configure<FormOptions>(options =>
+{
+    //options.MultipartBodyLengthLimit = 4_294_967_296; // 4 GB
+});
+// Upload file size limit on Kestrel
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    //options.Limits.MaxRequestBodySize = 4_294_967_296; // 4 GB
+});
+
+// SignalR max message size for Editor, FileManager, FileSelect, PDF Viewer, Signature
+builder.Services.Configure<HubOptions>(options =>
+{
+    //options.MaximumReceiveMessageSize = 64 * 1024 * 1024; // 64 MB or use null for no limit
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ TelerikBlazorWasm/TelerikBlazorWasm/Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// Upload file size limit on ASP.NET Core
+builder.Services.Configure<FormOptions>(options =>
+{
+    //options.MultipartBodyLengthLimit = 4_294_967_296; // 4 GB
+});
+// Upload file size limit on Kestrel
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    //options.Limits.MaxRequestBodySize = 4_294_967_296; // 4 GB
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
