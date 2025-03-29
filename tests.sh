87f0338fd805b429f12ab6c97e9a7447e6dfd86b
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2025-03-29 22:21:47 +0200

    Fix wildcard syntax in tests, CreateSolutionDirectory symbol in template.json

--- tests.sh
+++ tests.sh
@@ -9,6 +9,7 @@ cd ~/$testFolder
 
 echo
 echo ===== Web App Server, default settings =====
+echo dotnet new dimodi-blazor-server -o TestServerDefaultSettings
 
 dotnet new dimodi-blazor-server -o TestServerDefaultSettings
 cd TestServerDefaultSettings
@@ -19,8 +20,9 @@ rm -rf TestServerDefaultSettings
 
 echo
 echo ===== Web App Server, non-default settings =====
+echo dotnet new dimodi-blazor-server -o TestServerNonDefaultSettings -f net8.0 -tv "*" -th bootstrap -dm -c -it Font -lo -r -sms 64 -uc -do
 
-dotnet new dimodi-blazor-server -o TestServerNonDefaultSettings -f net8.0 -tv * -th bootstrap -dm -c -it Font -lo -r -sms 64 -uc -do
+dotnet new dimodi-blazor-server -o TestServerNonDefaultSettings -f net8.0 -tv "*" -th bootstrap -dm -c -it Font -lo -r -sms 64 -uc -do
 cd TestServerNonDefaultSettings
 dotnet build
 
@@ -29,6 +31,7 @@ rm -rf TestServerNonDefaultSettings
 
 echo
 echo ===== Web App WebAssembly, default settings =====
+echo dotnet new dimodi-blazor-wasmauto -o TestWasmDefaultSettings
 
 dotnet new dimodi-blazor-wasmauto -o TestWasmDefaultSettings
 cd TestWasmDefaultSettings
@@ -39,8 +42,9 @@ rm -rf TestWasmDefaultSettings
 
 echo
 echo ===== Web App WebAssembly, non-default settings =====
+echo dotnet new dimodi-blazor-wasmauto -o TestWasmNonDefaultSettings -f net8.0 -rm Auto -tv "*" -th bootstrap -dm -c -it Font -lo -r -uc -do
 
-dotnet new dimodi-blazor-wasmauto -o TestWasmNonDefaultSettings -f net8.0 -rm Auto -tv * -th bootstrap -dm -c -it Font -lo -r -uc -do
+dotnet new dimodi-blazor-wasmauto -o TestWasmNonDefaultSettings -f net8.0 -rm Auto -tv "*" -th bootstrap -dm -c -it Font -lo -r -uc -do
 cd TestWasmNonDefaultSettings
 dotnet build
 
@@ -49,6 +53,7 @@ rm -rf TestWasmNonDefaultSettings
 
 echo
 echo ===== WebAssembly Standalone, default settings =====
+echo dotnet new dimodi-blazorwasm -o TestWasmAloneDefaultSettings
 
 dotnet new dimodi-blazorwasm -o TestWasmAloneDefaultSettings
 cd TestWasmAloneDefaultSettings
@@ -59,8 +64,9 @@ rm -rf TestWasmAloneDefaultSettings
 
 echo
 echo ===== WebAssembly Standalone, non-default settings =====
+echo dotnet new dimodi-blazorwasm -o TestWasmAloneNonDefaultSettings -f net8.0 -tv "*" -th bootstrap -dm -c -it Font -lo -r -do
 
-dotnet new dimodi-blazorwasm -o TestWasmAloneNonDefaultSettings -f net8.0 -tv * -th bootstrap -dm -c -it Font -lo -r -do
+dotnet new dimodi-blazorwasm -o TestWasmAloneNonDefaultSettings -f net8.0 -tv "*" -th bootstrap -dm -c -it Font -lo -r -do
 cd TestWasmAloneNonDefaultSettings
 dotnet build
 
