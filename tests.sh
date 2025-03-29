2fb5817c4259a84af18c92905dc59d8930b3717f
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2025-03-29 21:51:01 +0200

    Refactor tests

---
+++ tests.sh
@@ -0,0 +1,68 @@
+if [ $# -eq 0 ]; then
+ echo "Error: Provide a path argument"
+ exit 1
+fi
+
+testFolder=$1
+
+cd ~/$testFolder
+
+echo
+echo ===== Web App Server, default settings =====
+
+dotnet new dimodi-blazor-server -o TestServerDefaultSettings
+cd TestServerDefaultSettings
+dotnet build
+
+cd ~/$testFolder
+rm -rf TestServerDefaultSettings
+
+echo
+echo ===== Web App Server, non-default settings =====
+
+dotnet new dimodi-blazor-server -o TestServerNonDefaultSettings -f net8.0 -tv * -th bootstrap -dm -c -it Font -lo -r -sms 64 -uc -do
+cd TestServerNonDefaultSettings
+dotnet build
+
+cd ~/$testFolder
+rm -rf TestServerNonDefaultSettings
+
+echo
+echo ===== Web App WebAssembly, default settings =====
+
+dotnet new dimodi-blazor-wasmauto -o TestWasmDefaultSettings
+cd TestWasmDefaultSettings
+dotnet build
+
+cd ~/$testFolder
+rm -rf TestWasmDefaultSettings
+
+echo
+echo ===== Web App WebAssembly, non-default settings =====
+
+dotnet new dimodi-blazor-wasmauto -o TestWasmNonDefaultSettings -f net8.0 -rm Auto -tv * -th bootstrap -dm -c -it Font -lo -r -uc -do
+cd TestWasmNonDefaultSettings
+dotnet build
+
+cd ~/$testFolder
+rm -rf TestWasmNonDefaultSettings
+
+echo
+echo ===== WebAssembly Standalone, default settings =====
+
+dotnet new dimodi-blazorwasm -o TestWasmAloneDefaultSettings
+cd TestWasmAloneDefaultSettings
+dotnet build
+
+cd ~/$testFolder
+rm -rf TestWasmAloneDefaultSettings
+
+echo
+echo ===== WebAssembly Standalone, non-default settings =====
+
+dotnet new dimodi-blazorwasm -o TestWasmAloneNonDefaultSettings -f net8.0 -tv * -th bootstrap -dm -c -it Font -lo -r -do
+cd TestWasmAloneNonDefaultSettings
+dotnet build
+
+cd ~/$testFolder
+rm -rf TestWasmAloneNonDefaultSettings
