fb95f190a8f69f909a6752cc3011d545b5b8340a
Dimo Dimov <961014+dimodi@users.noreply.github.com>
2025-06-07 22:51:34 +0300

    Add chunk upload controller method

--- Projects/Net8/TelerikBlazorWebApp/TelerikBlazorWebApp/Controllers/UploadController.cs
+++ Projects/Net8/TelerikBlazorWebApp/TelerikBlazorWebApp/Controllers/UploadController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 
 namespace TelerikBlazorWebApp.Controllers
 {
@@ -27,6 +30,40 @@ namespace TelerikBlazorWebApp.Controllers
 
                     using FileStream fs = new(saveLocation, FileMode.Create);
                     await files.CopyToAsync(fs);
+
+                    Response.StatusCode = 201;
+                }
+                catch (Exception ex)
+                {
+                    Response.StatusCode = 500;
+                    await Response.WriteAsync($"Upload failed: {ex.Message}");
+                }
+            }
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveChunk(IFormFile files, [FromForm] string chunkMetadata)
+        {
+            if (files != null)
+            {
+                try
+                {
+                    DataContractJsonSerializer dcSerializer = new(typeof(ChunkMetadata));
+                    MemoryStream ms = new(Encoding.UTF8.GetBytes(chunkMetadata));
+
+                    if (dcSerializer.ReadObject(ms) is not ChunkMetadata metadata)
+                    {
+                        throw new NullReferenceException("Chunk metadata serialization failed.");
+                    }
+
+                    string saveLocation = Path.Combine(RootPath, metadata.FileName);
+
+                    using FileStream fs = new(saveLocation, FileMode.Append);
+                    await files.CopyToAsync(fs);
+
+                    Response.StatusCode = 201;
                 }
                 catch (Exception ex)
                 {
@@ -61,5 +98,27 @@ namespace TelerikBlazorWebApp.Controllers
 
             return new EmptyResult();
         }
+    }
+
+    [DataContract]
+    public class ChunkMetadata
+    {
+        [DataMember(Name = "fileId")]
+        public string FileId { get; set; } = string.Empty;
+
+        [DataMember(Name = "fileName")]
+        public string FileName { get; set; } = string.Empty;
+
+        [DataMember(Name = "fileSize")]
+        public long FileSize { get; set; }
+
+        [DataMember(Name = "contentType")]
+        public string ContentType { get; set; } = string.Empty;
+
+        [DataMember(Name = "chunkIndex")]
+        public long ChunkIndex { get; set; }
+
+        [DataMember(Name = "totalChunks")]
+        public long TotalChunks { get; set; }
     }
 }
